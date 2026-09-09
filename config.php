2c90fe65dd7e2773bb5f65ec2a31af1c7898548c
Priyanon16 <66010912608@msu.ac.th>
2026-09-09 17:28:07 +0700

    a

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ai_center');
+define('DB_USER', 'root');
+define('DB_PASS', 'Took@66010912608');
+
+define('BASE_URL', 'http://119.10.140.166/reward');
+
+define('STAFF_PASSWORD', '1611');
+
+date_default_timezone_set('Asia/Bangkok');
